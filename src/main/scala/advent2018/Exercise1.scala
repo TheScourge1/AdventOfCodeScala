5c15f3087662a768f29721468efd0e2e1c78761f
Koen <penningmeester@badminton-pbo.be>
2018-12-27 17:59:24 +0100

    Excercise 1

--- src/main/scala/advent2018/Exercise1.scala
+++ src/main/scala/advent2018/Exercise1.scala
@@ -1,16 +1,34 @@
 package advent2018
 
+import collection.mutable
+
 object Exercise1 extends AdventBase{
 
   override def ex1(input: Seq[String]): String = {
     var result = 0
 
+    for(s <- input)  result += getValue(s)
     return ""+result
   }
 
   override def ex2(input: Seq[String]): String = {
     var result = 0
 
-    return ""+result
+    var resultSet = mutable.HashSet.empty[Int]
+    while(true) {
+      for (s <- input) {
+        result += getValue(s)
+        if (resultSet.contains(result)) return "" + result
+        else resultSet.add(result)
+      }
+    }
+
+    return "error"
+  }
+
+  def getValue(str: String): Int = {
+    if(str.startsWith("+")) return str.substring(1).toInt
+    else if(str.startsWith("-")) return  -1* str.substring(1).toInt
+    else throw new Exception("Unknown input data" + str)
   }
 }
--- src/test/scala/advent2018/Test1.scala
+++ src/test/scala/advent2018/Test1.scala
@@ -4,17 +4,19 @@ import org.scalatest.FunSuite
 
 class Test1 extends FunSuite {
 
-  val ex = Exercise1
+  private val ex = Exercise1
 
   test("Exercise1.ex1") {
-    assert(ex.ex1("inp") === "outp")
+    val input = Vector("+1","+1","+1")
+    assert(ex.ex1(input) === "3")
 
-    println("Solution 1: "+ex.ex1FromFile("ex1_1.txt"))
+    println("Solution 1: "+ex.ex1FromFile("2018/ex1_1.txt"))
   }
 
   test("Exercise1.ex2") {
-    assert(ex.ex2("inp") === "outp")
+    val input = Vector("+3","+3","+4","-2","-4")
+    assert(ex.ex2(input) === "10")
 
-    println("Solution 2: "+ex.ex2FromFile("ex1_1.txt"))
+    println("Solution 2: "+ex.ex2FromFile("2018/ex1_1.txt"))
   }
 }
