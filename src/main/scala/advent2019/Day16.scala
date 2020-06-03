52dbf013f256c1b62deb588e0ab6518fb0c311ed
u200561 <aappxx55>
2020-06-03 16:57:06 +0200

    Improve algorithm

--- src/main/scala/advent2019/Day16.scala
+++ src/main/scala/advent2019/Day16.scala
@@ -10,21 +10,18 @@ object Day16 extends Day(16){
     TestCase("02935109699940807407585447034323","78725270"),TestCase("03081770884921959731165446850517","53553731"))
 
   override def solutionA(input: List[String], params: List[String]) = {
-    val Iterations = 100
-    var sequence = input(0).split("").map(s =>s.toInt).toList
+    val Loops = 100
+    var sequence = input(0).split("").map(s =>s.toInt)
 
-    for(loop <- 0 until Iterations){
-      var newSequence = ListBuffer[Int]()
+    0 until Loops foreach{ _ =>
+      val newSequence = Array.ofDim[Int](sequence.size)
       for(n <- 1 to sequence.size) {
-        var newDigit = 0
         for(i <- 0 until sequence.size/n){
-          newDigit += sequence.slice(n-1+i*4*n, Math.min(2*n-1+i*4*n, sequence.size)).sum
-          newDigit -= sequence.slice(3*n-1+i*4*n, Math.min(3*n-1+i*4*n+n, sequence.size)).sum
+          for(t <- n-1+i*4*n until Math.min(2*n-1+i*4*n, sequence.size))  newSequence(n-1) += sequence(t)
+          for(t <- 3*n-1+i*4*n until Math.min(3*n-1+i*4*n+n, sequence.size))  newSequence(n-1) -= sequence(t)
         }
-        newSequence +=Math.abs(newDigit)%10
       }
-      sequence = newSequence.toList
-     // println(s"${loop}: - ${sequence}")
+      sequence = newSequence.map(f => Math.abs(f)%10)
     }
 
     sequence.foldLeft("")(_ + _).substring(0,8)
@@ -32,10 +29,47 @@ object Day16 extends Day(16){
 
   override def solutionB(input: List[String], params: List[String]) = {
 
-    "TODO"
+    val Loops = 100
+    var sequence = input(0).split("").map(s =>s.toInt)
+    sequence = Array.fill(10000)(sequence).flatten
+    val offset = input(0).substring(0,8).toInt
+
+    0 until Loops foreach{ a =>
+      val newSequence = Array.ofDim[Int](sequence.size)
+      for(n <- 1 to sequence.size) {
+        if(n%1000 ==0) println(s"${n} of ${sequence.size}")
+        for(i <- 0 until sequence.size/n){
+          for(t <- n-1+i*4*n until Math.min(2*n-1+i*4*n, sequence.size))  newSequence(n-1) += sequence(t)
+          for(t <- 3*n-1+i*4*n until Math.min(3*n-1+i*4*n+n, sequence.size))  newSequence(n-1) -= sequence(t)
+        }
+      }
+      sequence = newSequence.map(f => Math.abs(f)%10)
+      println(a)
+    }
+
+    sequence.foldLeft("")(_ + _).substring(offset,offset+8)
   }
 
+def slightlyFastersolutionA(input: List[String], params: List[String]): String = {
+  val Iterations = 100
+  var sequence = input(0).split("").map(s =>s.toInt).toList
 
+  for(loop <- 0 until Iterations){
+    var newSequence = ListBuffer[Int]()
+    for(n <- 1 to sequence.size) {
+      var newDigit = 0
+      for(i <- 0 until sequence.size/n){
+        newDigit += sequence.slice(n-1+i*4*n, Math.min(2*n-1+i*4*n, sequence.size)).sum
+        newDigit -= sequence.slice(3*n-1+i*4*n, Math.min(3*n-1+i*4*n+n, sequence.size)).sum
+      }
+      newSequence +=Math.abs(newDigit)%10
+    }
+    sequence = newSequence.toList
+    // println(s"${loop}: - ${sequence}")
+  }
+
+  sequence.foldLeft("")(_ + _).substring(0,8)
+}
 
   def oldSolutionA(input: List[String], params: List[String]) = {
     val BasePhase = List(0,1,0,-1)
