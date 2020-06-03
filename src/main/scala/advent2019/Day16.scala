9db888743b9b4250481e6992a5b83a7411f19fd6
Koen <penningmeester@badminton-pbo.be>
2020-06-03 23:11:01 +0200

    Day16 improvements

--- src/main/scala/advent2019/Day16.scala
+++ src/main/scala/advent2019/Day16.scala
@@ -1,5 +1,7 @@
 package advent2019
 
+import java.util.Calendar
+
 import scala.collection.mutable.ListBuffer
 
 object Day16 extends Day(16){
@@ -13,17 +15,7 @@ object Day16 extends Day(16){
     val Loops = 100
     var sequence = input(0).split("").map(s =>s.toInt)
 
-    0 until Loops foreach{ _ =>
-      val newSequence = Array.ofDim[Int](sequence.size)
-      for(n <- 1 to sequence.size) {
-        for(i <- 0 until sequence.size/n){
-          for(t <- n-1+i*4*n until Math.min(2*n-1+i*4*n, sequence.size))  newSequence(n-1) += sequence(t)
-          for(t <- 3*n-1+i*4*n until Math.min(3*n-1+i*4*n+n, sequence.size))  newSequence(n-1) -= sequence(t)
-        }
-      }
-      sequence = newSequence.map(f => Math.abs(f)%10)
-    }
-
+    sequence = calcFFT(sequence,Loops)
     sequence.foldLeft("")(_ + _).substring(0,8)
   }
 
@@ -32,50 +24,51 @@ object Day16 extends Day(16){
     val Loops = 100
     var sequence = input(0).split("").map(s =>s.toInt)
     sequence = Array.fill(10000)(sequence).flatten
-    val offset = input(0).substring(0,8).toInt
+    val offset = input(0).substring(0,7).toInt
 
-    0 until Loops foreach{ a =>
-      val newSequence = Array.ofDim[Int](sequence.size)
-      for(n <- 1 to sequence.size) {
-        if(n%1000 ==0) println(s"${n} of ${sequence.size}")
-        for(i <- 0 until sequence.size/n){
-          for(t <- n-1+i*4*n until Math.min(2*n-1+i*4*n, sequence.size))  newSequence(n-1) += sequence(t)
-          for(t <- 3*n-1+i*4*n until Math.min(3*n-1+i*4*n+n, sequence.size))  newSequence(n-1) -= sequence(t)
-        }
-      }
-      sequence = newSequence.map(f => Math.abs(f)%10)
-      println(a)
-    }
-
+    sequence = calcFFT(sequence,Loops)
     sequence.foldLeft("")(_ + _).substring(offset,offset+8)
   }
 
-def slightlyFastersolutionA(input: List[String], params: List[String]): String = {
-  val Iterations = 100
-  var sequence = input(0).split("").map(s =>s.toInt).toList
-
-  for(loop <- 0 until Iterations){
-    var newSequence = ListBuffer[Int]()
-    for(n <- 1 to sequence.size) {
-      var newDigit = 0
-      for(i <- 0 until sequence.size/n){
-        newDigit += sequence.slice(n-1+i*4*n, Math.min(2*n-1+i*4*n, sequence.size)).sum
-        newDigit -= sequence.slice(3*n-1+i*4*n, Math.min(3*n-1+i*4*n+n, sequence.size)).sum
+  def calcFFT(inputSequence: Array[Int],loops: Int): Array[Int] = {
+    val start = Calendar.getInstance().getTimeInMillis
+    var sequence = inputSequence
+    0 until loops foreach { a =>
+      val newSequence = Array.ofDim[Int](sequence.size)
+      for (n <- 1 to sequence.size) {
+        if (n % 100000 == 0) println(s"${n} of ${sequence.size} " + (Calendar.getInstance().getTimeInMillis - start) + "ms")
+        val size = sequence.size
+        for (i <- 0 until size / (4 * n)+1) {
+          for (t <- n - 1 + i * 4 * n until Math.min(2 * n - 1 + i * 4 * n, size)) newSequence(n - 1) += sequence(t)
+          for (t <- 3 * n - 1 + i * 4 * n until Math.min(3 * n - 1 + i * 4 * n + n, size)) newSequence(n - 1) -= sequence(t)
+        }
       }
-      newSequence +=Math.abs(newDigit)%10
+      sequence = newSequence.map(f => Math.abs(f) % 10)
+      println(a)
     }
-    sequence = newSequence.toList
-    // println(s"${loop}: - ${sequence}")
+    sequence
   }
 
-  sequence.foldLeft("")(_ + _).substring(0,8)
-}
+  def slightlyFastersolutionA(input: List[String], params: List[String]): String = {
+    val Iterations = 100
+    var sequence = input(0).split("").map(s =>s.toInt).toList
 
-  override def solutionB(input: List[String], params: List[String]) = {
+    for(loop <- 0 until Iterations){
+      var newSequence = ListBuffer[Int]()
+      for(n <- 1 to sequence.size) {
+        var newDigit = 0
+        for(i <- 0 until sequence.size/n){
+          newDigit += sequence.slice(n-1+i*4*n, Math.min(2*n-1+i*4*n, sequence.size)).sum
+          newDigit -= sequence.slice(3*n-1+i*4*n, Math.min(3*n-1+i*4*n+n, sequence.size)).sum
+        }
+        newSequence +=Math.abs(newDigit)%10
+      }
+      sequence = newSequence.toList
+      // println(s"${loop}: - ${sequence}")
+    }
 
-    "TODO"
+    sequence.foldLeft("")(_ + _).substring(0,8)
   }
-
 
 
   def oldSolutionA(input: List[String], params: List[String]) = {
