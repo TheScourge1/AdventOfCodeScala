f7cd95242b23607e8a97574b7831dd42814d32b1
Koen <penningmeester@badminton-pbo.be>
2020-08-19 22:16:09 +0200

    Day20 Part 2

--- src/main/scala/advent2019/Day20.scala
+++ src/main/scala/advent2019/Day20.scala
@@ -1,9 +1,11 @@
 package advent2019
 
-object Day20 extends Day(20){
-  override def testSetA = List(TestCase("Day20_testa1.txt","26"),TestCase("Day20_testa2.txt","58"))
+import scala.collection.mutable.ListBuffer
 
-  override def testSetB = List(TestCase("Day20_testa2.txt","58"),TestCase("Day20_testb1.txt","396"))
+object Day20 extends Day(20){
+  override def testSetA = List(TestCase("Day20_testa1.txt","23"),TestCase("Day20_testa2.txt","58"))
+
+  override def testSetB = List(TestCase("Day20_testa1.txt","26"),TestCase("Day20_testb1.txt","396"))
 
   def StringPattern= "[A-Z]{2}".r
 
@@ -14,8 +16,12 @@ object Day20 extends Day(20){
   }
 
   override def solutionB(input: List[String], params: List[String]) = {
-
-    "TODO"
+    val gridArr = readInputArr(input)
+    val portalEnds = getPortalEnds(gridArr)
+    var reachablePortalsEnds = Map[PortalEnd,List[(PortalEnd,Int)]]()
+    for(pe <- portalEnds) reachablePortalsEnds = reachablePortalsEnds + (pe -> getPortalEndsInReach(pe,gridArr))
+    val result = findShortestPathB(portalEnds.filter(p => p.name == "AA"),reachablePortalsEnds)
+    result._2.toString
   }
 
   def findShortestPath(path:List[String],track: List[(Int, Int)], grid: Array[Array[String]]): (List[String],Int) = {
@@ -27,19 +33,73 @@ object Day20 extends Day(20){
     for(m <- validMoves(grid,track)){
       var newPath = (List[String](),0)
       grid(m._1)(m._2) match {
-        case "ZZ" => return (path:+grid(m._1)(m._2),distance)
-        case isPortal() => newPath =  findShortestPath(path:+(grid(m._1)(m._2)+track.size), track :+m :+ findPortalTarget(m,grid),grid)
+        case "ZZ" => return (path:+grid(m._1)(m._2)+track.size,distance)
+        case isPortal() => newPath =  findShortestPath(path:+(grid(m._1)(m._2)+track.size),
+          track :+m :+ findPortalTarget(m,grid),grid)
         case "." => newPath = findShortestPath(path,track :+m,grid)
         case _ => throw new Exception(s"Invalid grid value found: ${grid(m._1)(m._2)} at ${m}")
       }
       val newMinVal = newPath._2
-      if(newMinVal > 0 && (minVal == 0 || minVal > newMinVal)) {
+      if(newMinVal > 0 && (minVal ==  0 || minVal > newMinVal)) {
         minVal = newMinVal
         minResult = newPath._1
       }
     }
 
     (minResult,minVal)
+  }
+
+  def getPortalEnds(grid: Array[Array[String]]): List[PortalEnd] = {
+    var result = ListBuffer[PortalEnd]()
+    for(i <- 0 until grid.size;j <- 0 until grid(0).size)
+      if(grid(i)(j).matches(StringPattern.regex)) {
+        val newPe = PortalEnd(grid(i)(j), i, j, grid)
+        result += newPe
+        if (newPe.name != "AA" && newPe.name != "ZZ") result += newPe.oppositePortalEnd()
+      }
+    result.toList
+  }
+
+  def getPortalEndsInReach(pe: PortalEnd , grid: Array[Array[String]]): List[(PortalEnd,Int)] = {
+    def doStep(path: List[(Int,Int)], grid: Array[Array[String]]): List[(PortalEnd,Int)] = {
+      var result = List[(PortalEnd,Int)]()
+      val isPortal = StringPattern
+
+      for(m <- validMoves(grid,path)){
+        grid(m._1)(m._2) match {
+          case isPortal() => result = result :+ (PortalEnd(grid(m._1)(m._2),m._1,m._2,grid),path.size)
+          case "." => result = result ++ doStep(path :+ m,grid)
+          case _ =>
+        }
+      }
+      result
+    }
+    doStep(List((pe.x,pe.y)),grid)
+  }
+
+  def findShortestPathB(path: List[PortalEnd], map: Map[PortalEnd,List[(PortalEnd,Int)]] ):(List[PortalEnd] ,Int)= {
+
+    val stack = VisitOptionsStack()
+    stack.pushPath(path,0)
+    while(true){
+      val nextOption = stack.popShortestPathOption()
+      val length = nextOption._1
+      val path = nextOption._2
+      val location = path.last
+
+      for(nextPortal <- map(location)){
+        if(depth(path) == 0 && nextPortal._1.name == "ZZ")
+          return (path :+ nextPortal._1,length+nextPortal._2)
+        else if(depth(path) <= 0 && nextPortal._1.name != "ZZ" && nextPortal._1.name != "AA")
+          stack.pushPath(path :+nextPortal._1.oppositePortalEnd,length+nextPortal._2+1)
+      }
+    }
+
+    (List(),-1)
+  }
+
+  def depth(path: List[PortalEnd]): Int = {
+    path.map(pe => if(pe.isOuterEdge) -1 else 1).sum +1
   }
 
   def validMoves(grid: Array[Array[String]],track: List[(Int,Int)]) : List[(Int,Int)] = {
@@ -57,6 +117,9 @@ object Day20 extends Day(20){
 
     result
   }
+
+  def onOuterEdge(m: (Int,Int),grid: Array[Array[String]]) =
+    m._1 == 0 || m._1 == grid.size-1 || m._2 == 0 || m._2 == grid(0).size-1
 
   def findLocation(token: String, grid: Array[Array[String]]): List[(Int,Int)] = {
     var result = List[(Int,Int)]()
@@ -133,5 +196,33 @@ object Day20 extends Day(20){
       res = res.append("\n")
     }
     res.toString
+  }
+
+  case class PortalEnd(name:String,x:Int,y:Int,grid:Array[Array[String]]) {
+    override def toString() = name + (if(isOuterEdge) "-1" else "1")
+    def oppositePortalEnd():PortalEnd = {
+      val coords = findPortalTarget((x,y),grid)
+      PortalEnd(name,coords._1,coords._2,grid)
+    }
+
+    def isOuterEdge: Boolean = onOuterEdge((x,y),grid)
+  }
+
+  case class VisitOptionsStack() {
+    private var map = Map[Int,List[List[PortalEnd]]]()
+
+    def popShortestPathOption():(Int,List[PortalEnd]) = {
+      val length = map.keys.min
+      val nextPath = map(length).head
+      if(map(length).size == 1)
+        map = map - length
+      else map = map + (length -> map(length).slice(1,nextPath.size))
+      (length,nextPath)
+    }
+
+    def pushPath(path: List[PortalEnd],length:Int) = {
+      val newPathList = map.getOrElse(length,List[List[PortalEnd]]()) :+ path
+      map = map + (length -> newPathList)
+    }
   }
 }
