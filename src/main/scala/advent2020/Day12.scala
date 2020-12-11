7eccd2dd5d15d140b0b0b6a4212300daedf87cd9
Koen <penningmeester@badminton-pbo.be>
2020-12-11 23:15:53 +0100

    Season 2020 Day 11

---
+++ src/main/scala/advent2020/Day12.scala
@@ -0,0 +1,19 @@
+package advent2020
+
+object Day12 extends Day{
+  override def day() = 12
+
+  override def testSetA = List(TestCase("",""))
+
+  override def testSetB = List(TestCase("",""))
+
+  override def solutionA(input: List[String], params: List[String]) = {
+
+    "TODO"
+  }
+
+  override def solutionB(input: List[String], params: List[String]) = {
+
+    "TODO"
+  }
+}
